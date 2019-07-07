43ea8f4eb02c9c882b9247d157c9e3075004d9d8
lukasikp <lukasik.paulina@gmail.com>
2019-07-07 13:34:53 +0200

    simple request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function loadGreeting() {
+  const response = await fetch("http://localhost:9000/graphql", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ query: "{greeting}" })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.greeting;
+}
+
+loadGreeting();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      greeting: ""
+    };
+    loadGreeting().then(greeting => this.setState({ greeting }));
+  }
+  render() {
+    const { greeting } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{greeting}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
